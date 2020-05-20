748663202a48c4ad8cd217ceeee2956bd0eeb5f1
robert <tanym@koal.com>
2020-05-21 00:45:49 +0800

    add module ca module

---
+++ openca_ca/src/test/java/com/robert/openca_ca/OpencaCaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robert.openca_ca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpencaCaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
